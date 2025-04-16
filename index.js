29e6e58f920bba784081ea718ca7fa57ebd8c1f5
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-16 16:38:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,19 @@ const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycby0jsj-E9qiug
 
 app.use(cors());
 app.use(express.json());
+
+// Servir archivos estáticos desde la raíz del proyecto
+app.use(express.static(__dirname));
+
+// Ruta raíz que carga "index.html"
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Ruta para redirigir a "registros.html"
+app.get("/registros", (req, res) => {
+    res.sendFile(path.join(__dirname, "registros.html"));
+});
 
 // Ruta para recibir datos del frontend y enviarlos a Google Sheets
 app.post("/registrar", async (req, res) => {
