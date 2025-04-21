0bdce1bd3aa7f26837ff26dc0160cd74d7d7bfb4
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-21 15:39:04 -0500

    Update registro.js

--- registro.js
+++ registro.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 
         try {
-            const response = await fetch("https://script.google.com/macros/s/TU_URL_DE_APPS_SCRIPT/exec", {
+            const response = await fetch("https://script.google.com/macros/s/AKfycbyQqqydA8Hqo0O-Cy9zluRtHih-th_HZej_7i9jEgayW0x3G80pcdF9HA6OaQxlTHXf/exec", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(pedido),
