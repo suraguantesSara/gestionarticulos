c32c08d306b4905809ce289e8ea7c76e4d6073a0
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-22 10:40:37 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 // 📌 URL del script de Google Sheets
-$GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbyKPO9HirAOGKdhJFbQclxQuhOAQhuxs_3-9vx5GODOXAy5ugjLgg2BpaNWETo4wBMN/exec";
+$GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbxjgM26a_h5B3myezn7el5OrFQ5MG3VybT8qQr4eS97RVs8O6A-S3VY8VySoE25jkYC/exec";
 
 // 📌 Mostrar mensaje confirmando conexión
 echo json_encode(["mensaje" => "✅ Conexión establecida con la base de datos (Google Sheets)."]);
