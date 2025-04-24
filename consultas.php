4d0fee9985bb8b7b0db2cab3ea96e0225581e450
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-24 12:27:25 -0500

    Update consultas.php

--- consultas.php
+++ consultas.php
@@ -3,25 +3,8 @@ require('fpdf.php');
 
 $filtro = $_GET['filtro'];
 $valor = $_GET['valor'];
-$data = json_decode(file_get_contents("<?php
-require('fpdf.php');
+$data = json_decode(file_get_contents("https://script.google.com/macros/s/AKfycbwqupaXl7Sxq-SqHLdwylOA6zEKTJZiLrLi1joIBFWeuu6ntZWney3HhbQQNPy35LYS/exec?filtro=".urlencode($filtro)."&valor=".urlencode($valor)), true);
 
-$filtro = $_GET['filtro'];
-$valor = $_GET['valor'];
-$data = json_decode(file_get_contents("https://script.google.com/macros/s/AKfycbwUXQuxraQtKEFuFvcfHHOU2Bz4xUIrGuhlfek2a5nnxoGg6wWyT1CvJM76HjzlZlDz/exec?filtro=".$filtro."&valor=".$valor), true);
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 16);
-$pdf->Cell(40, 10, "Informe de Consulta");
-
-$pdf->SetFont('Arial', '', 12);
-foreach ($data as $row) {
-    $pdf->Ln();
-    $pdf->Cell(40, 10, implode(", ", $row));
-}
-
-$pdf->Output();
-?>
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16);
