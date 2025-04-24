05e2b6db83424de181e8d0c06168330e65c448b5
suraguantesSara <aprendizsuraguantes@gmail.com>
2025-04-24 14:25:52 -0500

    Update consultas.php

--- consultas.php
+++ consultas.php
@@ -3,17 +3,27 @@ require('fpdf.php');
 
 $filtro = $_GET['filtro'];
 $valor = $_GET['valor'];
-$data = json_decode(file_get_contents("https://script.google.com/macros/s/AKfycbx0KD_5RFIsKWlnb5u8jYki0v2EYruS_gCnf0gdxX0mqX6Rzm3bJIkyfoG0PNnq6d7C/exec?filtro=".urlencode($filtro)."&valor=".urlencode($valor)), true);
+$usuario = $_GET['usuario'];
+$fecha_actual = date("Y-m-d H:i:s");
+
+$data = json_decode(file_get_contents("https://script.google.com/macros/s/AKfycbx91_RfojfKdstAFiYAqlesAST2M7M9iFkSvJ9NsNjkJcS-akxpX3YPdgrIjKY-H_SC/exec?filtro=".urlencode($filtro)."&valor=".urlencode($valor)), true);
 
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial', 'B', 16);
 $pdf->Cell(40, 10, "Informe de Consulta");
+$pdf->Ln();
 
 $pdf->SetFont('Arial', '', 12);
+$pdf->Cell(40, 10, "Fecha: " . $fecha_actual);
+$pdf->Ln();
+$pdf->Cell(40, 10, "Generado por: " . $usuario);
+$pdf->Ln();
+$pdf->Ln();
+
 foreach ($data as $row) {
-    $pdf->Ln();
     $pdf->Cell(40, 10, implode(", ", $row));
+    $pdf->Ln();
 }
 
 $pdf->Output();
